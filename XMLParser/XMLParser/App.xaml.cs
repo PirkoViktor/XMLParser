4d045acea66713e9a7b1201770ed46b258f58782
DESKTOP-FT778CM\corov <corovan1234578@gmail.com>
2017-03-02 00:06:18 +0200

    firtst Ui

--- XMLParser/XMLParser/App.xaml.cs
+++ XMLParser/XMLParser/App.xaml.cs
@@ -13,5 +13,6 @@ namespace XMLParser
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
---
+++ XMLParser/XMLParser/ViewModel/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿
+using System.ComponentModel;
+using System.Data;
+using System.Collections.ObjectModel;
+namespace XMLParser.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+    public class ViewModelWindow: ViewModelBase
+    {   
+        public ViewModelWindow()
+        {
+            this.TableHeaders= new ObservableCollection<string> { "a","b","c","d"};
+        }
+        public DataSet TableData { get; set; }
+        public ObservableCollection<string> TableHeaders { get; set; }
+    }
+}

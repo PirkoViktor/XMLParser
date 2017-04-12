9f227e8eb9fbdb9517b7ddb9c5a0e9951dee8aa9
PIRKO-VV\djosa <djosapirko@outlook.com>
2017-04-13 00:13:12 +0300

    dsdsffdsdsf

--- XMLParser/BDOperational/Class1.cs
+++ XMLParser/BDOperational/Class1.cs
@@ -7,102 +7,135 @@ using System.Data;
 using System.Data.SqlClient;
 namespace BDOperational
 {
-   
-        struct ConnectionSettings
+
+    public struct ConnectionSettings
+    {
+
+        public string Data_Source;
+        public string Initial_Catalog;
+        public string user_name;
+        public string password;
+
+    }
+
+
+    public class NorthwindDataSet
+    {
+        public DataSet selectTable(ConnectionSettings set)
+        {
+            string connectionString = "Data Source=" + set.Data_Source + "; " + "Initial Catalog=" +
+                                       set.Initial_Catalog + "; " + "Integrated Security = TRUE;";
+
+
+
+            using (SqlConnection connection =
+            new SqlConnection(connectionString))
+            {
+                string commandline = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG ='" + set.Initial_Catalog + "'";
+
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(commandline, connection);
+                DataSet ds = new DataSet();
+                try
+                {
+                    adapter.Fill(ds);
+                }
+                catch (Exception e)
+                {
+
+                }
+                connection.Close();
+                return ds;
+            }
+        }
+
+        public DataSet select(ConnectionSettings set, string table)
         {
 
-            public string Data_Source;
-            public string Initial_Catalog;
-            public string user_name;
+            string connectionString = "Data Source=" + set.Data_Source + "; " + "Initial Catalog=" +
+                                      set.Initial_Catalog + "; " + "Integrated Security = TRUE;";
+            return ConnectToData(connectionString, table);
+        }
+
+        public void insert(ConnectionSettings set, DataTable table)
+        {
+
+            string connectionString = "Data Source=" + set.Data_Source + ";  " + "Initial Catalog=" +
+                                      set.Initial_Catalog + ";" + "Integrated Security = TRUE;";
+            ConnectToData1(connectionString, table);
+        }
+
+        private static DataSet ConnectToData(string connectionString, string table)
+        {
+            using (SqlConnection connection =
+                 new SqlConnection(connectionString))
+            {
+                string commandline = "SELECT * FROM " + table;
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(commandline, connection);
+                DataSet ds = new DataSet();
+                try
+                {
+                    adapter.Fill(ds);
+                }
+                catch (Exception e)
+                {
+
+                }
+                connection.Close();
+                return ds;
+            }
 
         }
 
-
-        class NorthwindDataSet
+        private static void ConnectToData1(string connectionString, DataTable table)
         {
-            public void select(ConnectionSettings set, string table)
-            {
 
-                string connectionString = "Data Source=(" + set.Data_Source + "); " + "Initial Catalog=" + set.Initial_Catalog + ";" + "Integrated Security = SSPI";
-                ConnectToData(connectionString, table);
-            }
-            public void insert(ConnectionSettings set, DataTable table)
+            //Create a SqlConnection to the Northwind database.
+            using (SqlConnection connection =
+                new SqlConnection(connectionString))
             {
+                string commandline = "SELECT * FROM " + table.TableName;
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(commandline, connection);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
 
-                string connectionString = "Data Source=(" + set.Data_Source + "); " + "Initial Catalog=" + set.Initial_Catalog + ";" + "Integrated Security = SSPI";
-                ConnectToData1(connectionString, table);
-            }
 
-            private static DataSet ConnectToData(string connectionString, string table)
-            {
-                DataSet dataSet;
-                //Create a SqlConnection to the Northwind database.
-                using (SqlConnection connection =
-                           new SqlConnection(connectionString))
+                foreach (DataRow tab in table.Rows)
                 {
-                    //Create a SqlDataAdapter for the Suppliers table.
-                    SqlDataAdapter adapter = new SqlDataAdapter();
-
-                    // A table mapping names the DataTable.
-                    adapter.TableMappings.Add("Table", table);
-
-                    // Open the connection.
-                    connection.Open();
-                    string commandline = "SELECT * FROM " + table + '"';
-                    // Create a SqlCommand to retrieve Suppliers data.
-                    SqlCommand command = new SqlCommand(commandline, connection);
-                    command.CommandType = CommandType.Text;
-
-                    // Set the SqlDataAdapter's SelectCommand.
-                    adapter.SelectCommand = command;
-
-                    // Fill the DataSet.
-                    dataSet = new DataSet(table);
-                    adapter.Fill(dataSet);
-
-                    // Close the connection.
-                    connection.Close();
-
-                }
-                return dataSet;
-            }
-            private static void ConnectToData1(string connectionString, DataTable table)
-            {
-
-                //Create a SqlConnection to the Northwind database.
-                using (SqlConnection connection =
-                           new SqlConnection(connectionString))
-                {
-                    string commandline = "SELECT * FROM " + table.TableName + '"';
-                    connection.Open();
-                    SqlDataAdapter adapter = new SqlDataAdapter(commandline, connection);
-                    DataSet ds = new DataSet();
-                    adapter.Fill(ds);
-
-                    DataTable dt = ds.Tables[0];
-
-                    foreach (var tab in table.Rows)
+                    try
                     {
-                        dt.Rows.Add(tab);
+                        ds.Tables[0].ImportRow(tab);
                     }
-                    // создаем объект SqlCommandBuilder
-                    SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
-                    adapter.Update(ds);
-                    // альтернативный способ - обновление только одной таблицы
-                    //adapter.Update(dt);
-                    // заново получаем данные из бд
-                    // очищаем полностью DataSet
-                    ds.Clear();
-                    // перезагружаем данные
-                    adapter.Fill(ds);
+                    catch (Exception e)
+                    {
 
+                    }
+                }
+                // создаем объект SqlCommandBuilder
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+
+                try
+                {
+                    adapter.Update(ds);
+                }
+                catch (Exception e)
+                {
 
                 }
+                // альтернативный способ - обновление только одной таблицы
+                //adapter.Update(dt);
+                // заново получаем данные из бд
+                // очищаем полностью DataSet
+                ds.Clear();
+                // перезагружаем данные
+                adapter.Fill(ds);
+                connection.Close();
+
 
             }
+
         }
-    
-
-
-
+    }
 }
